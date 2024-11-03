e6a7291be42f7ff13bbfb7dc0fd27a75f73056f1
dvir.monajem <dmenajem@gmail.com>
2024-11-03 16:32:05 +0200

    click event output works

--- src/app/float-icon-button/float-icon-button.component.ts
+++ src/app/float-icon-button/float-icon-button.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from "@angular/common";
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
   Input,
-  OnInit,
+  Output
 } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 
@@ -12,7 +13,13 @@ import { MatIconModule } from "@angular/material/icon";
   selector: "app-float-icon-button",
   standalone: true,
   imports: [CommonModule, MatButtonModule, MatIconModule],
-  templateUrl: "./float-icon-button.component.html",
+  template: `<button
+    mat-fab
+    (click)="clickEvent.emit()"
+    [ngStyle]="buttonStyles"
+    aria-label="Floating action button">
+    <mat-icon [fontIcon]="icon"></mat-icon>
+  </button> `,
   styleUrls: ["./float-icon-button.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -20,6 +27,8 @@ export class FloatIconButtonComponent {
   @Input() icon: string = "add";
   @Input() verticalPosition: "top" | "bottom" = "bottom";
   @Input() horizontalPosition: "left" | "right" = "right";
+
+  @Output() clickEvent = new EventEmitter<void>();
 
   get buttonStyles() {
     return {
