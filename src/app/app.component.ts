87da56fd208d433129c67e3f8118fa15afe0f38e
dvir.monajem <dmenajem@gmail.com>
2024-11-03 19:25:37 +0200

    add userService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,25 @@ import { Component, inject } from "@angular/core";
 import { UserDialogService } from "./components/user-dialog/user-dialog.service";
 import { UserFormComponent } from "./components/user-form/user-form.component";
 import { FloatIconButtonComponent } from "./shared/float-icon-button/float-icon-button.component";
+import { User } from "./models/user";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [FloatIconButtonComponent, UserFormComponent],
+  imports: [CommonModule, FloatIconButtonComponent, UserFormComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "matrix-user-manager";
+  user: User[] = [{
+    age: 10,
+    city: "Tek aviv",
+    country: "hungary",
+    firstName: "Dvir",
+    gender: "female",
+    lastName: "Dom",
+  }];
 
   #dialogService = inject(UserDialogService);
 
