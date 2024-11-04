c4e236e6ffdbb2d653f488f6ac2c70fc3195bc34
dvir.monajem <dmenajem@gmail.com>
2024-11-04 07:15:30 +0200

    🦄 refactor(float-button-icon): Add Dynamic
    add getters and setters + remove getter from buttonStyles

--- src/app/shared/float-icon-button/float-icon-button.component.ts
+++ src/app/shared/float-icon-button/float-icon-button.component.ts
@@ -4,7 +4,8 @@ import {
   Component,
   EventEmitter,
   Input,
-  Output
+  OnInit,
+  Output,
 } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
@@ -23,18 +24,52 @@ import { MatIconModule } from "@angular/material/icon";
   styleUrls: ["./float-icon-button.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FloatIconButtonComponent {
-  @Input() icon: string = "add";
-  @Input() verticalPosition: "top" | "bottom" = "bottom";
-  @Input() horizontalPosition: "left" | "right" = "right";
+export class FloatIconButtonComponent implements OnInit {
+  #icon: string = "add";
+  #verticalPosition: "top" | "bottom" = "bottom";
+  #horizontalPosition: "left" | "right" = "right";
 
   @Output() clickEvent = new EventEmitter<void>();
 
-  get buttonStyles() {
-    return {
+  buttonStyles!: { [key: string]: string };
+
+  @Input() set icon(value: string) {
+    this.#icon = value;
+    this.updateButtonStyles();
+  }
+  get icon(): string {
+    return this.#icon;
+  }
+
+  @Input() set verticalPosition(value: "top" | "bottom") {
+    this.#verticalPosition = value;
+    this.updateButtonStyles();
+  }
+  get verticalPosition(): "top" | "bottom" {
+    return this.#verticalPosition;
+  }
+
+  @Input() set horizontalPosition(value: "left" | "right") {
+    this.#horizontalPosition = value;
+    this.updateButtonStyles();
+  }
+  get horizontalPosition(): "left" | "right" {
+    return this.#horizontalPosition;
+  }
+
+  ngOnInit() {
+    this.updateButtonStyles();
+  }
+
+  private updateButtonStyles(): void {
+    this.buttonStyles = {
       position: "fixed",
-      [this.verticalPosition]: "20px",
-      [this.horizontalPosition]: "20px",
+      [this.#verticalPosition]: "20px",
+      [this.#horizontalPosition]: "20px",
     };
+  }
+
+  onClick(): void {
+    this.clickEvent.emit();
   }
 }
