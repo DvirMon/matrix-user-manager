fbf545bc841ba189b2bb23a2e9cab84f8452a74d
dvir.monajem <dmenajem@gmail.com>
2024-11-04 13:28:32 +0200

    🦄 refactor(APP): Lazy Load
    lazy load hone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,89 +1,11 @@
-import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
-import { UserDialogService } from "./components/user-dialog/user-dialog.service";
-import { UserFormComponent } from "./components/user-form/user-form.component";
-import { User } from "./models/user";
-import { FloatIconButtonComponent } from "./shared/float-icon-button/float-icon-button.component";
-import { UsersService } from "./services/users.service";
-import { filter, Observable, Subject, switchMap, takeUntil, tap } from "rxjs";
-import { UserTableComponent } from "./components/user-table/user-table.component";
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    CommonModule,
-    FloatIconButtonComponent,
-    UserFormComponent,
-    UserTableComponent,
-  ],
+  imports: [RouterModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  #dialogService = inject(UserDialogService);
-
-  #userService = inject(UsersService);
-
-  users$ = this.#userService.getUsers$();
-
-  addSubject = new Subject<void>();
-  editSubject = new Subject<User>();
-
-  addEvent$: Observable<User>;
-  editEvent$: Observable<User>;
-
-  columns = [
-    { key: "firstName", header: "First Name" },
-    { key: "lastName", header: "Last Name" },
-    { key: "gender", header: "Gender" },
-    { key: "age", header: "Age" },
-    { key: "country", header: "Country" },
-    { key: "city", header: "City" },
-  ];
-
-  constructor() {
-    this.addEvent$ = this.#onAddEvent();
-    this.editEvent$ = this.#onEditEvent();
-  }
-
-  #onAddEvent() {
-    return this.addSubject.asObservable().pipe(
-      switchMap(() =>
-        this.#dialogService
-          .open({ mode: "add", user: null })
-          .afterClosed()
-          .pipe(
-            filter((user) => user !== null),
-            tap((user: User) => this.#userService.addUser(user))
-          )
-      )
-    );
-  }
-
-  #onEditEvent() {
-    return this.editSubject.asObservable().pipe(
-      switchMap((user) =>
-        this.#dialogService
-          .open({ mode: "edit", user })
-          .afterClosed()
-          .pipe(
-            filter((user) => user !== null),
-            tap((user: User) => this.#userService.editUser(user.id, user))
-          )
-      )
-    );
-  }
-
-  onClickEvent(): void {
-    this.addSubject.next();
-  }
-
-  onEditTableEvent(user: User): void {
-    this.editSubject.next(user);
-  }
-
-  onDeleteEditEvent(user: User): void {
-    this.#userService.deleteUser(user.id);
-  }
-}
+export class AppComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,92 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { filter, Observable, Subject, switchMap, tap } from "rxjs";
+import { UserDialogService } from "src/app/components/user-dialog/user-dialog.service";
+import { UserFormComponent } from "src/app/components/user-form/user-form.component";
+import { UserTableComponent } from "src/app/components/user-table/user-table.component";
+import { User } from "src/app/models/user";
+import { UsersService } from "src/app/services/users.service";
+import { FloatIconButtonComponent } from "src/app/shared/float-icon-button/float-icon-button.component";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  imports: [
+    NgIf,
+    AsyncPipe,
+    RouterModule,
+    FloatIconButtonComponent,
+    UserFormComponent,
+    UserTableComponent,
+  ],
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  #dialogService = inject(UserDialogService);
 
-  constructor() { }
+  #userService = inject(UsersService);
 
-  ngOnInit(): void {
+  users$ = this.#userService.getUsers$();
+
+  addSubject = new Subject<void>();
+  editSubject = new Subject<User>();
+
+  addEvent$: Observable<User>;
+  editEvent$: Observable<User>;
+
+  columns = [
+    { key: "firstName", header: "First Name" },
+    { key: "lastName", header: "Last Name" },
+    { key: "gender", header: "Gender" },
+    { key: "age", header: "Age" },
+    { key: "country", header: "Country" },
+    { key: "city", header: "City" },
+  ];
+
+  constructor() {
+    this.addEvent$ = this.#onAddEvent();
+    this.editEvent$ = this.#onEditEvent();
   }
 
+  #onAddEvent() {
+    return this.addSubject.asObservable().pipe(
+      switchMap(() =>
+        this.#dialogService
+          .open({ mode: "add", user: null })
+          .afterClosed()
+          .pipe(
+            filter((user) => user !== null),
+            tap((user: User) => this.#userService.addUser(user))
+          )
+      )
+    );
+  }
+
+  #onEditEvent() {
+    return this.editSubject.asObservable().pipe(
+      switchMap((user) =>
+        this.#dialogService
+          .open({ mode: "edit", user })
+          .afterClosed()
+          .pipe(
+            filter((user) => user !== null),
+            tap((user: User) => this.#userService.editUser(user.id, user))
+          )
+      )
+    );
+  }
+
+  onClickEvent(): void {
+    this.addSubject.next();
+  }
+
+  onEditTableEvent(user: User): void {
+    this.editSubject.next(user);
+  }
+
+  onDeleteEditEvent(user: User): void {
+    this.#userService.deleteUser(user.id);
+  }
 }
