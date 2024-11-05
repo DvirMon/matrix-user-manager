2bc4cf186934110cc453194474ce81cc0d949a0c
dvir.monajem <dmenajem@gmail.com>
2024-11-05 17:14:33 +0200

    remote user server works

--- src/app/services/http-storage.service.ts
+++ src/app/services/http-storage.service.ts
@@ -1,43 +1,54 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import {
+  Observable,
+  of,
+  shareReplay,
+  startWith,
+  Subject,
+  switchMap,
+} from "rxjs";
 import { User } from "../models/user";
-import { v4 as uuidv4 } from "uuid";
 
 @Injectable({
   providedIn: "root",
 })
 export class HttpStorageService {
   readonly apiUrl = "http://localhost:3000/users";
-  #usersSubject = new BehaviorSubject<User[]>([]);
-  users$ = this.#usersSubject.asObservable();
+  #reloadTrigger$ = new Subject<void>();
+
+  #users$: Observable<User[]> = this.#reloadTrigger$.asObservable().pipe(
+    startWith(void 0),
+    switchMap(() => this.#http.get<User[]>(this.apiUrl)),
+    shareReplay(1)
+  );
 
   #http = inject(HttpClient);
 
-  loadUsers(): void {
-    this.#http.get<User[]>(this.apiUrl).subscribe(users => {
-      this.#usersSubject.next(users);
-    });
-  }
-
   getUsers$(): Observable<User[]> {
-    return this.#http.get<User[]>(this.apiUrl);
+    return this.#users$;
   }
 
-  addUser(user: Partial< User>): Observable<User> {
-    const userWithId = { ...user, id: uuidv4() };
-
-    return this.#http.post<User>(this.apiUrl, userWithId);
+  addUser(user: User): Observable<void> {
+    return this.#http
+      .post<User>(this.apiUrl, user)
+      .pipe(switchMap(() => this.#triggerReload()));
   }
 
   deleteUser(userId: string): Observable<void> {
-    return this.#http.delete<void>(`${this.apiUrl}/${userId}`);
+    return this.#http
+      .delete<void>(`${this.apiUrl}/${userId}`)
+      .pipe(switchMap(() => this.#triggerReload()));
   }
 
-  editUser(updatedUserData: Partial<User>): Observable<User> {
-    return this.#http.patch<User>(
-      `${this.apiUrl}/${updatedUserData.id}`,
-      updatedUserData
-    );
+  editUser(updatedUserData: Partial<User>): Observable<void> {
+    return this.#http
+      .patch<User>(`${this.apiUrl}/${updatedUserData.id}`, updatedUserData)
+      .pipe(switchMap(() => this.#triggerReload()));
+  }
+
+  #triggerReload(): Observable<void> {
+    this.#reloadTrigger$.next(); // Trigger the reload
+    return of();
   }
 }
