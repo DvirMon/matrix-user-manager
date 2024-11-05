0a77c6b2c38d3d7177580f477fe3ec7bf1feca40
dvir.monajem <dmenajem@gmail.com>
2024-11-05 14:16:03 +0200

    update logic with ngControl

--- src/app/directives/option-validation.directive.ts
+++ src/app/directives/option-validation.directive.ts
@@ -1,34 +1,44 @@
-import { Directive, Input } from "@angular/core";
+import { Directive, inject, Input } from "@angular/core";
 import {
   AbstractControl,
-  NG_VALIDATORS,
+  NgControl,
   ValidationErrors,
-  ValidatorFn
+  ValidatorFn,
+  Validators
 } from "@angular/forms";
-import { countryMatchValidator } from "../components/user-form/user-form.service";
 
+export function matchValidator<T>(validOptions: T[] | null): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value || !validOptions) return null; // Allow empty values
+
+    const isValid = validOptions.includes(control.value as T);
+    return isValid ? null : { optionMismatch: true };
+  };
+}
 @Directive({
   selector: "[appOptionValidation]",
   standalone: true,
-  providers: [
-    {
-      provide: NG_VALIDATORS,
-      useExisting: OptionValidationDirective,
-      multi: true,
-    },
-  ],
 })
 export class OptionValidationDirective {
+  #list: Array<string | number | boolean> | null = null;
   #validator: ValidatorFn | null = null;
 
+  #ngControl = inject(NgControl, { optional: true });
+
   @Input("list")
-  set list(value: string[] | null) {
-    if (value !== null) {
-      this.#validator = countryMatchValidator(value as string[]);
-    }
+  set list(value: Array<string | number | boolean> | null) {
+    this.#list = value;
+    this.#validator = matchValidator(this.#list);
+    this.applyValidator();
   }
 
-  validate(control: AbstractControl): ValidationErrors | null {
-    return this.#validator ? this.#validator(control) : null;
+  private applyValidator() {
+    if (this.#ngControl?.control && this.#validator) {
+      this.#ngControl.control.setValidators([
+        Validators.required,
+        this.#validator,
+      ]);
+      this.#ngControl.control.updateValueAndValidity();
+    }
   }
 }
