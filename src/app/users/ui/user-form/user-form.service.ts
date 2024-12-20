2332cb70b6d604831fe88d15ba8bf747968d68aa
dvir.monajem <dmenajem@gmail.com>
2024-12-20 11:39:37 +0200

    use typescript utils for form typing

--- src/app/users/ui/user-form/user-form.service.ts
+++ src/app/users/ui/user-form/user-form.service.ts
@@ -1,33 +1,40 @@
 import { inject, Injectable } from "@angular/core";
-import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import {
+  FormControl,
+  NonNullableFormBuilder,
+  Validators
+} from "@angular/forms";
 import { User } from "src/app/users/data-access/user";
 
+type UserFormControls = {
+  [K in keyof Omit<User, 'id'>]: FormControl<User[K]>;
+};
 @Injectable({
   providedIn: "root",
 })
 export class UserFormService {
-  #fbn = inject(NonNullableFormBuilder);
+  #nfb = inject(NonNullableFormBuilder);
 
   createUserForm(user: User) {
-    return this.#fbn.group({
-      firstName: [
-        user.firstName || "",
-        [Validators.required, Validators.pattern(/^[a-zA-Z\u0590-\u05FF\s]*$/)],
-      ],
-      lastName: [
-        user.lastName || "",
-        [Validators.required, Validators.pattern(/^[a-zA-Z\u0590-\u05FF\s]*$/)],
-      ],
-      age: [
-        user.age || null,
-        [Validators.required, Validators.pattern(/^[0-9]*$/)],
-      ],
-      city: [
-        user.city || "",
-        [Validators.required, Validators.pattern(/^[a-zA-Z\u0590-\u05FF\s]*$/)],
-      ],
-      gender: [user.gender || "", Validators.required],
-      country: [user.country || "", Validators.required],
+    return this.#nfb.group<UserFormControls>({
+      firstName: this.#nfb.control(user.firstName || "", [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\u0590-\u05FF\s]*$/),
+      ]),
+      lastName: this.#nfb.control(user.lastName || "", [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\u0590-\u05FF\s]*$/),
+      ]),
+      age: this.#nfb.control(user.age || 0, [
+        Validators.required,
+        Validators.pattern(/^[0-9]*$/),
+      ]),
+      city: this.#nfb.control(user.city || "", [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\u0590-\u05FF\s]*$/),
+      ]),
+      gender: this.#nfb.control(user.gender || "", Validators.required),
+      country: this.#nfb.control(user.country || "", Validators.required),
     });
   }
 }
